4820339b782b7b49224619d4129bce26bfe15f64
Deepkamal yadav <dkits1995@gmail.com>
2023-01-20 23:40:19 +0530

    dashboard

---
+++ frontend/src/Deepkamal/Singleproduct_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Singleproduct_page = () => {
+  return (
+    <div>Singleproduct_page</div>
+  )
+}
+
+export default Singleproduct_page
---
+++ frontend/src/Deepkamal/update.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const update = () => {
+  return (
+    <div>update</div>
+  )
+}
+
+export default update
